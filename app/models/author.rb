7d2bb8c3de42963e66021f225e774b21f8849839
cgdev <sharma.paras4444@gmail.com>
2022-10-31 13:48:52 +0530

    Added validations to Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,8 @@
 
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
+
+  validates :first_name, presence: true, length: { minimum: 3 }
+  validates :last_name, presence: true, length: { minimum: 3 }
+  validates :age, presence: true
 end
